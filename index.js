1ce9ab3cdf4c35bc5432207f0b9b4071da6b780f
Yulia Masliak <1985kozak@gmail.com>
2023-06-05 08:32:27 +0300

    fix bug

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-app.use(cors());
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', function (req, res) {
